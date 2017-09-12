5164823101be626ba93b6d951c1ec060ea41eb1e
Simon Arvidsson <arsi1632@student.ju.se>
2017-09-12 13:52:55 +0200

    Implemented scene & view. Changed class 'Hashlife' from QMainWindow to QWidget.

--- hashlife.h
+++ hashlife.h
@@ -1,22 +1,32 @@
 #ifndef HASHLIFE_H
 #define HASHLIFE_H
 #include "gamelogic.h"
-#include <QMainWindow>
+#include <QWidget>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QVBoxLayout>
+#include <iostream>
 
 namespace Ui {
 class Hashlife;
 }
 
-class Hashlife : public QMainWindow
+class Hashlife : public QWidget
 {
     Q_OBJECT
 
 public:
     explicit Hashlife(QWidget *parent = 0);
+
     ~Hashlife();
 
 private:
+    void resizeEvent(QResizeEvent *event) override;
+    void autoScaleView();
+
     Ui::Hashlife *ui;
+    QGraphicsScene* _mainScene;
+    QGraphicsView* _mainView;
     GameLogic _logic;
 };
 
