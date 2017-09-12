502e7b9c8a47741d2f87f1ee7753a738294607c5
Filip Andersson <anfi1622@student.ju.se>
2017-09-12 10:04:22 +0200

    nodes

--- node.h
+++ node.h
@@ -1,13 +1,48 @@
 #ifndef NODE_H
 #define NODE_H
-
 #include "gridobject.h"
+
+
+enum class Direction
+{
+    none = 0,top,right,bottom,left
+};
+
+//
+class dirConections
+{
+public:
+    dirConections(int value);
+
+
+private:
+    nodeConnect _leftValues[2];
+    nodeConnect _topValues[2];
+    nodeConnect _rightValues[2];
+    nodeConnect _bottomValues[2];
+};
+
+class nodeConnect
+{
+public:
+    nodeConnect();
+
+private:
+    Direction _mySide;
+    Direction _targetSide;
+    Node* _target;
+    Node* _me;
+};
+
 
 class Node : public GridObject
 {
 public:
     Node();
     void paintMember(QPainter &painter);
+
+    //wip
+    void connectNodes(Direction side);
 private:
 
 int _value;
